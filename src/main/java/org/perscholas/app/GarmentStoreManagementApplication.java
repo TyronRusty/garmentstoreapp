d489c776c78545701f5d417aa2f5ebc3e488a222
TyronRusty <Ktyred@gmail.com>
2023-02-21 01:29:03 -0500

    init commit for garmentstore app

---
+++ src/main/java/org/perscholas/app/GarmentStoreManagementApplication.java
@@ -0,0 +1,13 @@
+package org.perscholas.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarmentStoreManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarmentStoreManagementApplication.class, args);
+	}
+
+}
